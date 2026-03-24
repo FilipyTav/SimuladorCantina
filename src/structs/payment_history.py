9529fc5a52f7110b3f5a4e5bbaa885a7a9c82ae0
Filipy <filipytav17@gmail.com>
2026-03-24 20:33:16 -0300

    Encapsulates ledger better

--- src/structs/payment_history.py
+++ src/structs/payment_history.py
@@ -23,11 +23,11 @@ class PaymentNode:
 # Singly linked list
 class PaymentLedger:
     def __init__(self):
-        self.head: PaymentNode | None = None
-        self.tail: PaymentNode | None = None
-        self.count: int = 0
+        self.__head: PaymentNode | None = None
+        self.__tail: PaymentNode | None = None
+        self.__count: int = 0
 
-        self.report_info: ReportData = {
+        self.__report_info: ReportData = {
             "by_category": {},
             "by_course": {},
             "by_item": {},
@@ -37,31 +37,31 @@ class PaymentLedger:
         }
 
     def insert_at(self, p: Payment, pos: int) -> bool:
-        if pos < 0 or pos > self.count:
-            print(f"Index out of range: {pos}, the list has {self.count} element(s)")
+        if pos < 0 or pos > self.__count:
+            print(f"Index out of range: {pos}, the list has {self.__count} element(s)")
             return False
 
         new_node: PaymentNode = PaymentNode(p)
 
         # Empty
         if self.is_empty():
-            self.head = self.tail = new_node
-            self.count += 1
+            self.__head = self.__tail = new_node
+            self.__count += 1
             return True
 
         # new_node is now the head
         if pos == 0:
-            new_node.next = self.head
+            new_node.next = self.__head
 
-            self.head = new_node
+            self.__head = new_node
         # new_node is now the tail
-        elif pos == self.count:
-            assert self.tail
-            self.tail.next = new_node
+        elif pos == self.__count:
+            assert self.__tail
+            self.__tail.next = new_node
 
-            self.tail = new_node
+            self.__tail = new_node
         else:
-            current: PaymentNode | None = self.head
+            current: PaymentNode | None = self.__head
 
             for _ in range(pos - 1):
                 if not current:
@@ -75,37 +75,37 @@ class PaymentLedger:
             new_node.next = current.next
             current.next = new_node
 
-        self.count += 1
+        self.__count += 1
         return True
 
     def is_empty(self) -> bool:
-        return not (self.head and self.tail) or self.count == 0
+        return not (self.__head and self.__tail)
 
     def push(self, p: Payment) -> bool:
         cat, course = p.get_client_category(), p.get_client_course()
         val: int = p.get_value()
         prods: dict[int, int] = p.get_items()
 
-        self.report_info["by_category"][cat] = (
-            self.report_info["by_category"].get(cat, 0) + val
+        self.__report_info["by_category"][cat] = (
+            self.__report_info["by_category"].get(cat, 0) + val
         )
-        self.report_info["by_course"][course] = (
-            self.report_info["by_course"].get(course, 0) + val
+        self.__report_info["by_course"][course] = (
+            self.__report_info["by_course"].get(course, 0) + val
         )
 
         for pid, amount in prods.items():
-            self.report_info["by_item"][pid] = (
-                self.report_info["by_item"].get(pid, 0) + amount
+            self.__report_info["by_item"][pid] = (
+                self.__report_info["by_item"].get(pid, 0) + amount
             )
 
-        self.report_info["total_prods"] += sum(prods.values())
-        self.report_info["total_profit"] += val
-        self.report_info["total_transactions"] += 1
+        self.__report_info["total_prods"] += sum(prods.values())
+        self.__report_info["total_profit"] += val
+        self.__report_info["total_transactions"] += 1
 
-        return self.insert_at(p, self.count)
+        return self.insert_at(p, self.__count)
 
     def print_admin(self) -> None:
-        if not self.head:
+        if not self.__head:
             print("[!] O livro razão está vazio. Nenhuma venda registrada. [!]\n")
             return
 
@@ -116,7 +116,7 @@ class PaymentLedger:
         )
         print("-" * markers)
 
-        current: Pnode = self.head  # type: ignore
+        current: Pnode = self.__head  # type: ignore
         total: int = 0
 
         while current:
@@ -144,11 +144,11 @@ class PaymentLedger:
         )
         print("-" * markers)
         print(f"{'FATURAMENTO TOTAL:':>65} {total_final}")
-        print(f"{'TOTAL DE VENDAS:':>65} {self.count}")
+        print(f"{'TOTAL DE VENDAS:':>65} {self.__count}")
         print("=" * markers + "\n")
 
     def print_report(self) -> None:
-        data: ReportData = self.report_info
+        data: ReportData = self.__report_info
         markers: int = 45
 
         # --- Header ---
@@ -169,15 +169,15 @@ class PaymentLedger:
         print(f"    Renda média/venda:  {f'R${atv_fmt}':>15}")
         print("-" * markers)
 
-        self.display_sub_report(
+        self._display_sub_report(
             "Renda por Categoria", data["by_category"], is_money=True
         )
-        self.display_sub_report("Renda por Curso", data["by_course"], is_money=True)
-        self.display_sub_report("Quantidade por Item", data["by_item"], is_money=False)
+        self._display_sub_report("Renda por Curso", data["by_course"], is_money=True)
+        self._display_sub_report("Quantidade por Item", data["by_item"], is_money=False)
 
         print("=" * markers + "\n")
 
-    def display_sub_report(self, title: str, mapping: dict, is_money: bool) -> None:
+    def _display_sub_report(self, title: str, mapping: dict, is_money: bool) -> None:
         if not mapping:
             return
 
@@ -188,7 +188,7 @@ class PaymentLedger:
             print(f"  • {str(key).upper():<20} : {display_val:>15}")
 
     def save_report_graph(self, filename: str = "report.png") -> None:
-        data: ReportData = self.report_info
+        data: ReportData = self.__report_info
 
         # 3 subplots (1 row, 3 columns)
         fig, axs = plt.subplots(1, 3, figsize=(18, 6))
@@ -222,11 +222,11 @@ class PaymentLedger:
         print(f"Gráfico salvo com sucesso como: {filename}")
 
     def __repr__(self) -> str:
-        if not self.head:
+        if not self.__head:
             return "PaymentLedger: [Vazio]"
 
         items = []
-        current = self.head
+        current = self.__head
 
         while current:
             items.append(repr(current.data))
