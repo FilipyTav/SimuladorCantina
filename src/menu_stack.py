131e2979bc97b2b0bf3f89a2405e43c99ce69773
Filipy <filipytav17@gmail.com>
2026-03-24 19:40:27 -0300

    Implements menu stack structure

--- src/menu_stack.py
+++ src/menu_stack.py
@@ -1,24 +1,43 @@
 from ui import Screen
 
+
 class MNode:
-    def __init__(self):
-        self.data: Screen | None = None
-        self.next: Mnode | None = None
+    def __init__(self, data: Screen):
+        self.data: Screen | None = data
+        self.prev: MNode | None = None
+
 
 class MenuStack:
     def __init__(self):
-        self.top: MNode = None
+        self.top: MNode | None = None
 
         self.count: int = 0
-    
+
     def is_empty(self) -> bool:
         return not self.top and self.count <= 0
-    
+
     def clear(self) -> None:
-        pass
+        self.top = None
+        self.count = 0
 
-    def push(self) -> None:
-        pass
+    def push(self, screen: Screen) -> None:
+        new_node: MNode = MNode(screen)
 
-    def pop(self) -> None:
-        pass
+        new_node.prev = self.top
+        self.top = new_node
+        self.count += 1
+
+    def pop(self) -> Screen | None:
+        if self.is_empty():
+            return
+
+        assert self.top
+        popped: MNode = self.top
+
+        self.top = popped.prev
+        self.count -= 1
+        return popped.data
+
+    def peek(self) -> Screen | None:
+        return self.top.data if self.top else None
+
