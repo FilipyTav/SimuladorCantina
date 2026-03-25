caf82f1710f568e7e3833ed57ca524d4862f6f19
Filipy <filipytav17@gmail.com>
2026-03-25 09:10:17 -0300

    Improves save/load functions

--- src/utils/save_data.py
+++ src/utils/save_data.py
@@ -1,4 +1,6 @@
 import pickle
+import os
+import pathlib
 
 from structs.payment_history import PaymentLedger
 from structs.pqueue import PQueue
@@ -6,12 +8,13 @@ from typing import TypeAlias
 
 StructsSaved: TypeAlias = tuple[PQueue, PQueue, PaymentLedger]
 
-savefile: str = "structs.pkl"
-savepath: str = "../"
-fullpath: str = savepath + savefile
+BASE_DIR: pathlib.Path = pathlib.Path(__file__).parent.resolve()
+fullpath: pathlib.Path = BASE_DIR.parent / "structs.pkl"
 
 def save_structs(structs: StructsSaved) -> bool:
+    """stock, prods_available, ledger"""
     try:
+        fullpath.parent.mkdir(parents=True, exist_ok=True)
         with open(fullpath, 'wb') as f:
             pickle.dump(structs, f)
         return True
@@ -20,9 +23,10 @@ def save_structs(structs: StructsSaved) -> bool:
         return False
 
 def load_structs() -> StructsSaved | None:
+    """stock, prods_available, ledger"""
     try:
         with open(fullpath, 'rb') as f:
             return pickle.load(f)
     except FileNotFoundError:
-        print(f"Arquivo {savefile} não encontrado.")
+        print(f"Arquivo não encontrado em {fullpath}.")
         return None
